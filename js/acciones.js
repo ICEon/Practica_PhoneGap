0c721126e47f657e8eefb8b43ab2b41ad5f8d2b0
Eon Matrix <matrix_eon@yahoo.com>
2014-05-30 06:08:57 -0500

    Uno

---
+++ js/acciones.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+	var panel ='<div data-role="panel" id="mipanel" data-theme="a" data-display="push"><div class="panel-content" align="center"> <h3>Menu</h3> <div data-role="controlgroup"> <a data-role="button" href="#Principal">Principal</a> <a data-role="button" href="#Vibrar">Vibrar</a>  <a data-role="button" href="#Beep">Beep</a> <a data-role="button" >Swipe</a> </div> </div> </div>';
+		
+		$(document).on('pagebeforecreate', '[data-role=page]', function () {
+  if ($(this).find('[data-role=panel]').length === 0) {
+    $('[data-role=header]').before(panel);
+  }
+});
+
+$(document).ready(function(e) {
+  document.addEventListener("deviceready",function(){
+	  
+	$("#B_Vibrar").tap(function (){
+	  navigator.notification.vibrate(500);
+	 }); //tap B_Vibrar
+	  
+  }); //device ready
+}); //document ready
