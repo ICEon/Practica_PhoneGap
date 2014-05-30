23abca1f181634f0faf12d9f5033d9e1dbd77a1d
Eon Matrix <matrix_eon@yahoo.com>
2014-05-30 06:53:45 -0500

    vibration add

--- js/acciones.js
+++ js/acciones.js
@@ -1,12 +1,14 @@
 // JavaScript Document
-/*	var panel ='<div data-role="panel" id="mipanel" data-theme="a" data-display="push"><div class="panel-content" align="center"> <h3>Menu</h3> <div data-role="controlgroup"> <a data-role="button" href="#Principal">Principal</a> <a data-role="button" href="#Vibrar">Vibrar</a>  <a data-role="button" href="#Beep">Beep</a> <a data-role="button" >Swipe</a> </div> </div> </div>';
+	var panel ='<div data-role="panel" id="mipanel" data-theme="a" data-display="push"><div class="panel-content" align="center"> <h3>Menu</h3> <div data-role="controlgroup"> <a data-role="button" href="#Principal">Principal</a> <a data-role="button" href="#Vibrar">Vibrar</a>  <a data-role="button" href="#Beep">Beep</a> <a data-role="button" >Swipe</a> </div> </div> </div>';
 		
 		$(document).on('pagebeforecreate', '[data-role=page]', function () {
   if ($(this).find('[data-role=panel]').length === 0) {
     $('[data-role=header]').before(panel);
   }
 });
-*/
+
+
+
 $(document).ready(function(e) {
 
 document.addEventListener("deviceready",function(){
